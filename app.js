db187bcd28feb2764cc7157f4be44e13612aa24a
joshua-hurn <joshua.hurn@hotmail.com>
2018-09-18 16:55:28 -0500

    Started JQuery

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 $(document).ready(function () {
-    const input = $("input")
-    $("#btnSubmit").click(function () {
-        alert(input.val())
-        input.on('input', function () {
-            
-        })
-    })
+    const input = $("#mytextBox");
+    const btnSubmit = $("#btnSubmit");
+    $('body').append('<div></div>');
+    input.on("change paste keyup", function () {
+        if (input.val().length > 0) {
+            btnSubmit.attr('disabled', false);
+        } else {
+            btnSubmit.attr('disabled', true);
+        }
+    });
+        
+            });
+        });
+
+    });
 });
+
+   // btnSubmit.click(function (e) {
+    //     e.preventDefault();
+    //     let h2text = input.val();
+    //     $('<h2>' + h2text + '</h2>').appendTo('div');
+    //     $("h2").mouseover(function () {
+    //         $('h2').css({
+    //             "background-color": "green",
+    //             "border-radius": "1em",
